f1a0c1c146dcce9c183557b77a73de4c0685d837
pmwatt <prachnachai.mek@student.mahidol.edu>
2023-12-30 23:42:04 +0700

    fixed inline quotes for I was born at a very young age

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
     <>
         <Image className='rounded-full' src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
       <h1 className='text-5xl p-5'>About Me</h1>
-      <h2 className='text-3xl pt-12 pb-2'>"I was born at a very young age."</h2>
+      <h2 className='text-3xl pt-12 pb-2'>I was born at a very young age.</h2>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla esse maxime quos impedit veniam hic nisi labore, voluptates ut repellat blanditiis, sequi nesciunt maiores necessitatibus enim incidunt cumque dolorem quasi.</p>
       <h2 className='text-3xl pt-12 pb-2'>One of the most of all time.</h2>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla esse maxime quos impedit veniam hic nisi labore, voluptates ut repellat blanditiis, sequi nesciunt maiores necessitatibus enim incidunt cumque dolorem quasi.</p>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <>
       <h1 className='text-5xl p-5'>Home Page</h1>
-      <h2 className='text-3xl pt-12 pb-2'>"I was born at a very young age."</h2>
+      <h2 className='text-3xl pt-12 pb-2'>I was born at a very young age.</h2>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla esse maxime quos impedit veniam hic nisi labore, voluptates ut repellat blanditiis, sequi nesciunt maiores necessitatibus enim incidunt cumque dolorem quasi.</p>
     </>
   )
