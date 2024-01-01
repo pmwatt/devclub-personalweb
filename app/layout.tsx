a1c08aebd70c438076881d9862a475432a01ecc6
pmwatt <prachnachai.mek@student.mahidol.edu>
2024-01-01 16:14:14 +0700

    Refactored navbar and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,29 @@ export const metadata: Metadata = {
   description: 'A very professional website',
 }
 
+function NavBar() {
+  return (
+    <>
+      <nav>
+        <ul className='flex gap-4 p-5'>
+          <li><a className=' hover:bg-gray-800 py-3 px-6 m-1 rounded-full' href="/">Home</a></li>
+          <li><a className=' hover:bg-gray-800 py-3 px-6 m-1 rounded-full' href="/about">About</a></li>
+          <li><a className=' hover:bg-gray-800 py-3 px-6 m-1 rounded-full' href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+function Footer() {
+  return (
+    <>
+      <footer className='py-20'>
+        <p className='text-center'>Copyright 2023 Prachnachai Meakpaiboonwattana All Rights Reserved</p>
+      </footer>
+    </>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -19,21 +42,15 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={myFont.className}>
-        <div className='bg-black text-white h-full'>
-          <nav>
-            <ul className='flex gap-4 p-5'>
-              <li><a className=' hover:bg-gray-800 py-3 px-6 m-1 rounded-full' href="/">Home</a></li>
-              <li><a className=' hover:bg-gray-800 py-3 px-6 m-1 rounded-full' href="/about">About</a></li>
-              <li><a className=' hover:bg-gray-800 py-3 px-6 m-1 rounded-full' href="/contact">Contact</a></li>
-            </ul>
-          </nav>
-          <div className='flex flex-col items-center m-10'>
-            <Providers>{children}</Providers>
+        <Providers>
+          <div className='bg-black text-white h-full'>
+            <NavBar />
+            <div className='flex flex-col items-center m-10'>
+              {children}
+            </div>
+            <Footer />
           </div>
-          <footer className='py-20'>
-            <p className='text-center'>Copyright 2023 Prachnachai Meakpaiboonwattana All Rights Reserved</p>
-          </footer>
-        </div>
+        </Providers>
       </body>
     </html>
   )
